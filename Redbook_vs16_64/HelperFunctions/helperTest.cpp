7e8f7a9113529d2c76da13fcd7471bdd96dc5c3e
AAA <li2185@purdue.edu>
2020-06-05 06:54:50 -0400

    add helper project

---
+++ Redbook_vs16_64/HelperFunctions/helperTest.cpp
@@ -0,0 +1,12 @@
+#ifndef _GL_DEF
+#include <GLFW/glfw3.h>
+#include <GL/gl3w.h>
+#endif // !_GL_DEF
+#include <helper_functions.h>
+
+using namespace std;
+
+int main ( int argc, char** argv )
+{
+
+}
