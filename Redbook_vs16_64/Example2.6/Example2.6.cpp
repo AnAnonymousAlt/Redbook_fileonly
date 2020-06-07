e25627c3518305eb8d11b7b70bc9e40798dc94d9
AAA <li2185@purdue.edu>
2020-06-07 15:20:47 -0400

    Update Example2.6.cpp

--- Redbook_vs16_64/Example2.6/Example2.6.cpp
+++ Redbook_vs16_64/Example2.6/Example2.6.cpp
@@ -1,3 +1,5 @@
+
+
 #include <helper_functions.h>
 // to eliminate error
 void func (){
@@ -10,5 +12,39 @@ void func (){
 	glUseProgram ( program );
 
 	materialShaderLoc = glGetSubroutineUniformLocation (
-	program);
+	program, GL_VERTEX_SHADER, "materialShader");
+
+	if ( materialShaderLoc < 0 )
+	{
+		// Error: materialShader is not an active subroutine
+		// uniform in the shader
+	}
+
+	ambientIndex = glGetSubroutineIndex ( program,
+										  GL_VERTEX_SHADER,
+										  "ambient" );
+	diffuseIndex = glGetSubroutineIndex ( program,
+										  GL_VERTEX_SHADER,
+										  "diffuse" );
+
+	if ( ambientIndex == GL_INVALID_INDEX ||
+		 diffuseIndex == GL_INVALID_INDEX )
+	{
+		// Error: the specified subroutiens are not active 
+		// in the currently bound program for teh GL_VERTEX_SHADER
+		// stage.
+	}
+	else
+	{
+		GLsizei n;
+		glGetIntegerv ( GL_MAX_SUBROUTINE_UNIFORM_LOCATIONS, &n );
+
+		GLuint *indices = new GLuint[n];
+		indices[materialShaderLoc] = ambientIndex;
+
+		glUniformSubroutinesuiv ( GL_VERTEX_SHADER, n, indices );
+
+		delete[] indices;
+
+	}
 }
